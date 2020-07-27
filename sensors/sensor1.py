f400a1140dccb197855f274a75781ac4624a77aa
Sushant <sushantk2912@gmail.com>
2020-07-27 22:05:47 +0530

    sds

--- sensors/sensor1.py
+++ sensors/sensor1.py
@@ -15,8 +15,10 @@ class System_Sensor(Sensor):
     def run(self):
         while not self._stop:
             self._logger.debug('System_Sensor dispatching trigger')
-            payload = {'Monitoring': 'Monitoring operation performed on system'}
+            count = self.sensor_service.get_value('new_proj.count') or 0
+            payload = {'Monitoring': 'Monitoring operation performed on system','count': int(count) + 1}
             self.sensor_service.dispatch(trigger='new_proj.event1', payload=payload)
+            self.sensor_service.set_value('new_proj.count', payload['count'])
             eventlet.sleep(60)
 
     def cleanup(self):
