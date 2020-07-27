11e160ce0f2dce9633b476e703bc6aba81520f88
Sushant <sushantk2912@gmail.com>
2020-07-28 01:14:32 +0530

    project_final

--- sensors/sensor1.py
+++ sensors/sensor1.py
@@ -1,10 +1,10 @@
-import eventlet
 import os
+
 from st2reactor.sensor.base import PollingSensor
 
 
 class System_Sensor(PollingSensor):
-    def __init__(self, sensor_service, config,poll_interval=5):
+    def __init__(self, sensor_service, config,poll_interval=300):
         super(System_Sensor, self).__init__(sensor_service=sensor_service, config=config,poll_interval=poll_interval)
         self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
         self._stop = False
@@ -15,7 +15,7 @@ class System_Sensor(PollingSensor):
     def poll(self):
       
             self._logger.debug('System_Sensor dispatching trigger')
-            payload = {'greeting': 'Yo, StackStorm!'}
+            payload = {'Monitoring': 'Monitoring the System'}
             self.sensor_service.dispatch(trigger='new_proj.event1', payload=payload)
             
            
