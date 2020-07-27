32b08e836c5dbed15ca30192bf66a9f91cee283f
Sushant <sushantk2912@gmail.com>
2020-07-28 01:53:44 +0530

    sdsd

--- sensors/sensor1.py
+++ sensors/sensor1.py
@@ -4,10 +4,10 @@ from st2reactor.sensor.base import PollingSensor
 
 
 class System_Sensor(PollingSensor):
-    def __init__(self, sensor_service, config,poll_interval=5):
+    def __init__(self, sensor_service, config,poll_interval=300):
         super(System_Sensor, self).__init__(sensor_service=sensor_service, config=config,poll_interval=poll_interval)
         self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
-        self._stop = False
+
 
     def setup(self):
         pass
