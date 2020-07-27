02dc9362a99dbb096d250fee12e009b0b6eede9b
Sushant <sushantk2912@gmail.com>
2020-07-27 19:53:19 +0530

    dsd

--- sensors/sensor1.py
+++ sensors/sensor1.py
@@ -3,7 +3,7 @@ import eventlet
 from st2reactor.sensor.base import Sensor
 
 
-class HelloSensor(Sensor):
+class System_Sensor(Sensor):
     def __init__(self, sensor_service, config):
         super(HelloSensor, self).__init__(sensor_service=sensor_service, config=config)
         self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
@@ -14,17 +14,15 @@ class HelloSensor(Sensor):
 
     def run(self):
         while not self._stop:
-            self._logger.debug('HelloSensor dispatching trigger...')
-            count = self.sensor_service.get_value('new_proj.count') or 0
-            payload = {'greeting': 'Yo, StackStorm!', 'count': int(count) + 1}
+            self._logger.debug('System_Sensor dispatching trigger')
+            payload = {'Monitoring': 'Monitoring operation performed on system'}
             self.sensor_service.dispatch(trigger='new_proj.event1', payload=payload)
-            self.sensor_service.set_value('new_proj.count', payload['count'])
-            eventlet.sleep(60)
+            eventlet.sleep(600)
 
     def cleanup(self):
         self._stop = True
 
-    # Methods required for programmable sensors.
+   
     def add_trigger(self, trigger):
         pass
 
