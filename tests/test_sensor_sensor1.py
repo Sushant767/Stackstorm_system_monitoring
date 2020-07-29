c8e9aba217f8d733651a32d270728b1f75a2bea6
Sushant <sushantk2912@gmail.com>
2020-07-29 14:10:16 +0530

    ss

---
+++ tests/test_sensor_sensor1.py
@@ -0,0 +1,16 @@
+import mock
+
+from st2tests.base import BaseSensorTestCase
+
+
+from sensor1 import System_Sensor
+
+class System_SensorTestCase(BaseSensorTestCase):
+    sensor_cls = System_Sensor
+
+  def test_poll(self):
+        sensor = self.get_sensor_instance()
+
+        sensor.poll()
+        self.assertTriggerDispatched(trigger='new_proj.event1',
+                                     payload={'Monitoring': 'Monitoring the System'})
