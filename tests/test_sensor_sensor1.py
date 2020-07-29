6f80622d6d77daf36ac9f9e742adb0916bce056b
Sushant <sushantk2912@gmail.com>
2020-07-29 14:31:15 +0530

    sdsd

--- tests/test_sensor_sensor1.py
+++ tests/test_sensor_sensor1.py
@@ -9,7 +9,7 @@ class System_SensorTestCase(BaseSensorTestCase):
     sensor_cls = System_Sensor
 
     def test_poll(self):
-        sensor = self.get_sensor_instance()
+        sensor = self.get_sensor_instance(config={'foo': 'bar'})
 
         sensor.poll()
         self.assertTriggerDispatched(trigger='new_proj.event1',
